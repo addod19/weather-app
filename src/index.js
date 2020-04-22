48f444ef5b83775b90acf9b303d6d177c8a627ea
Daniel <addodaniellarbi@gmail.com>
2020-04-22 13:04:46 +0000

    add webpack installation

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 const api = ((key, url) => ({ key, url }));
 
+const root = document.getElementById('root');
+root.className = 'hello';
+root.innerText = 'Hello guys';
+console.log(root);
 const myApi = api('ssdkffgtgrewefrkothk', 'https://blahblahblah.com/blah/blah');
 console.log(myApi.url);
