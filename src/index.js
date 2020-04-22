9571701d5f3e50320b271ac6cd0637d9e81cb19c
Daniel <addodaniellarbi@gmail.com>
2020-04-22 12:48:32 +0000

    fix eslint initial error

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-const api = (key, url) => { 
-  return { key, url };
-};
+const api = ((key, url) => ({ key, url }));
 
 const myApi = api('ssdkffgtgrewefrkothk', 'https://blahblahblah.com/blah/blah');
 console.log(myApi.url);
