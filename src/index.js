074a4945eae23d0ea1c33e384c298f739f622c56
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2020-04-27 05:43:59 -0400

    add nav to ui

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import data from './data';
 import ui from './ui';
 
 
-const ontroller = ( (data, ui) => {
+const controller = ( (data, ui) => {
+
+//   const weatherData;
+//   const unit = 'F';
   console.log('hi it is me');
   data('Accra');
 })(data, ui);
--- src/ui.js
+++ src/ui.js
@@ -3,16 +3,35 @@ import first from '../dist/images/first.jpeg';
 const ui = ( () => {
   const root = document.getElementById('root');
 
+  const nav = document.createElement('NAV');
+  nav.className = 'nav custom-nav';
+  nav.innerText = 'Weather App';
+
   const main = document.createElement('DIV');
   main.className = 'container';
   main.style.background = first;
   console.log(main);
 
-  root.appendChild(main);
+  root.append(nav, main);
 
   const upper = document.createElement('DIV');
+  upper.className = 'row col-12';
+  upper.setAttribute('id', 'apiData');
 
   const lower = document.createElement('DIV');
+  lower.className = 'row col-12';
+
+  const searchInput = document.createElement('INPUT');
+  searchInput.setAttribute('placeholder', 'enter city');
+  searchInput.setAttribute('id', 'search');
+  searchInput.className = 'form-control';
+
+  const searchBtn = document.createElement('button');
+  searchBtn.className = 'btn btn-primary form-control mt-4';
+  searchBtn.innerText = 'Search';
+  searchBtn.setAttribute('id', 'btnSearch');
+
+  lower.append(searchInput, searchBtn);
 
 
   main.append(upper, lower);
