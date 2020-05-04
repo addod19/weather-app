61f8f7d10a1024856ed51bc36a654aaa08abbaa4
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2020-05-04 15:06:27 +0000

    fix lint errors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import data from './data';
 import ui from './ui';
-
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-confusing-arrow */
+/* eslint-disable no-unused-expressions */
 const controller = ((data, ui) => {
   let wD;
   let units = 'F';
-  const toggleUnits = (s) => units == 'F' ? 'imperial' : 'metric';
+  const toggleUnits = (units) => (units === 'F' ? 'imperial' : 'metric');
   const searchVal = document.getElementById('searchInput');
 
+  const resetValue = (searchVal) => {
+    searchVal.value = '';
+  };
+
+  /* eslint-disable consistent-return */
   const defaultWeather = async (city = 'Accra', unit = 'imperial') => {
     try {
       const result = await data.getWeather(city, unit);
@@ -14,33 +21,27 @@ const controller = ((data, ui) => {
       wD = await result;
       return result;
     } catch (e) {
-      console.log('sorry we could not find your city');
+      return e;
     }
-   
   };
 
   const handleClick = (event) => {
     if (event.target.id === 'searchInput') {
       resetValue(searchVal);
-    } else if (event.target.id === 'toggeleUnits'){
-        event.peventDefault;
-        console.log(wD.main);    
-        units = units === 'F' ? 'C' : 'F';
-        defaultWeather(wD.name, toggleUnits(units));
+    } else if (event.target.id === 'toggeleUnits') {
+      event.peventDefault;
+      units = units === 'F' ? 'C' : 'F';
+      defaultWeather(wD.name, toggleUnits(units));
     } else if (searchVal.value) {
-        defaultWeather(searchVal.value, toggleUnits(units));
-        resetValue(searchVal);
+      defaultWeather(searchVal.value, toggleUnits(units));
+      resetValue(searchVal);
     }
   };
 
   const myKeyPress = (event) => {
-    if (searchVal.value && event.which == 13) {
+    if (searchVal.value && event.which === 13) {
       defaultWeather(searchVal.value, toggleUnits(units));
     }
-  }
-
-  const resetValue = (searchVal) => {
-    searchVal.value = '';
   };
 
   defaultWeather();
--- src/ui.js
+++ src/ui.js
@@ -1,11 +1,11 @@
-const ui = (() => { 
+/* eslint-disable camelcase */
+const ui = (() => {
   const renderPage = async (weather, unit = 'F') => {
-    
     const {
       feels_like, temp, temp_min, temp_max,
     } = weather.main;
 
-    const { main, description} = weather.weather[0];
+    const { main, description } = weather.weather[0];
     const { name } = weather;
     const curDate = Date();
 
@@ -13,10 +13,9 @@ const ui = (() => {
     document.getElementById('name').innerText = `${name}, ${weather.sys.country} - ${Math.round(temp)}º${unit}`;
     document.getElementById('description').innerText = `${description.toUpperCase()} - ${main}`;
     document.getElementById('feels').innerText = `Feels like: ${Math.round(feels_like)} º${unit}`;
-    document.getElementById('temp').innerText = `Min temp ${Math.round(temp_min)} º${unit} - Max temp ${Math.round(temp_max)} º${unit}`
+    document.getElementById('temp').innerText = `Min temp ${Math.round(temp_min)} º${unit} - Max temp ${Math.round(temp_max)} º${unit}`;
     document.getElementById('toggeleUnits').innerText = `Change to º${
       unit === 'C' ? 'F' : 'C'}`;
-    
   };
 
   return {
